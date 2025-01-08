21e5a3d3138b8a1a802ca7ed63f33af52265afe0
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-01-08 18:47:24 +0600

    api integration

---
+++ src/redux/apiSlices/consultationSlice.ts
@@ -0,0 +1,39 @@
+import api from '../api/baseApi';
+
+const consultationApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getConsultationCategory: builder.query({
+      query: () => ({
+        url: '/category',
+        method: 'GET',
+      }),
+    }),
+    getConsultationCategoryById: builder.query({
+      query: (id) => ({
+        url: `/category/${id}`,
+        method: 'GET',
+      }),
+    }),
+    createConsultationCategory: builder.mutation({
+      query: (data) => ({
+        url: '/category/create',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    editConsultationCategory: builder.mutation({
+      query: ({ data, id }: { data: any; id: any }) => ({
+        url: `/category/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetConsultationCategoryQuery,
+  useGetConsultationCategoryByIdQuery,
+  useCreateConsultationCategoryMutation,
+  useEditConsultationCategoryMutation,
+} = consultationApi;
