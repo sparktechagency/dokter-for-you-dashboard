f0a7662391109723a8b4006e025907c940ad70e9
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-07-05 17:13:52 +0600

    updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,9 +4,9 @@ import { getFromLocalStorage, getSessionStorage } from '../../utils/local-storag
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://api.dokterforyou.com/api/v1',
+    baseUrl: 'https://api.dokterforyou.com/api/v1',
     // baseUrl: 'http://10.0.70.30:5002/api/v1',
-    baseUrl: 'http://152.42.140.58:5002/api/v1',
+    // baseUrl: 'http://152.42.140.58:5002/api/v1',
     // baseUrl: 'http://10.0.70.208:5002/api/v1',
     // baseUrl: 'http://152.42.140.58:5000/api/v1',
     // baseUrl: 'http://139.59.0.25:5002/api/v1',
