fb3c019d3e19fe541bbc02a28334f61e428eebf2
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-07-09 16:26:57 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '152.42.140.58',
+    host: '62.72.33.222',
     // port: 4174,
   },
 });
